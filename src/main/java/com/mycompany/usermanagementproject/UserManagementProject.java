e363fa3426b8ab953fe8906fb63ab2e260567ee9
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-08-15 21:32:44 +0700

    ✨ create project

---
+++ src/main/java/com/mycompany/usermanagementproject/UserManagementProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usermanagementproject;
+
+/**
+ *
+ * @author USER
+ */
+public class UserManagementProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
