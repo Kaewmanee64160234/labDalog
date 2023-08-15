209ed5b25f90c60029492487319ae48063639296
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-08-15 21:48:38 +0700

    create object User

--- src/main/java/com/mycompany/usermanagementproject/UserManagementProject.java
+++ src/main/java/com/mycompany/usermanagementproject/UserManagementProject.java
@@ -11,6 +11,12 @@ package com.mycompany.usermanagementproject;
 public class UserManagementProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        User admin = new User("admin", "Aministrator", "pass@1234", 'M', 'A');        
+        User user1 = new User("user1", "User 1", "pass@1234", 'M', 'U');        
+        User user2 = new User("user2", "User 2", "pass@1234", 'F', 'U');
+       
+
+
+
     }
 }
