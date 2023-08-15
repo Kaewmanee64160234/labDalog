e5158f7fc43bed4dc02876c2ea5e73706bc06721
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-08-15 22:27:22 +0700

    finish List

--- src/main/java/com/mycompany/usermanagementproject/UserManagementProject.java
+++ src/main/java/com/mycompany/usermanagementproject/UserManagementProject.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.usermanagementproject;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -11,12 +12,36 @@ package com.mycompany.usermanagementproject;
 public class UserManagementProject {
 
     public static void main(String[] args) {
-        User admin = new User("admin", "Aministrator", "pass@1234", 'M', 'A');        
-        User user1 = new User("user1", "User 1", "pass@1234", 'M', 'U');        
+        User admin = new User("admin", "Aministrator", "pass@1234", 'M', 'A');
+        User user1 = new User("user1", "User 1", "pass@1234", 'M', 'U');
         User user2 = new User("user2", "User 2", "pass@1234", 'F', 'U');
-       
-
-
+        System.out.println(admin.toString());
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
+        User[] userArr = new User[3];
+        userArr[0] = admin;
+        userArr[1] = user1;
+        userArr[2] = user2;
+         System.out.println("userArr");
+        for (User user : userArr) {
+            System.out.println(user.toString());
+        }
+        System.out.println("ArrayList");
+        ArrayList<User> userList = new ArrayList<User>();
+        userList.add(admin);
+        System.out.println(userList.get(userList.size()-1)+" list size ="+userList.size());
+        userList.add(user1);
+          System.out.println(userList.get(userList.size()-1)+" list size ="+userList.size());
+        userList.add(user2);
+          System.out.println(userList.get(userList.size()-1)+" list size ="+userList.size());
+        System.out.println("----------------------------------");
+        for (int i = 0; i < userList.size(); i++) {
+            System.out.println(userList.get(i));
+            
+        }
+        User user4 = new User("user4", "User 4", "pass@1234", 'M', 'U');
+        userList.set(0, user4);
+        System.out.println(userList.get(0));
 
     }
 }
